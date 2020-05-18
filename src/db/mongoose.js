88e2965eb3ecfaa68f92a7f5787ce836180f7716
Osama Zahid <usamaansari@yahoo.com>
2020-05-18 14:39:12 +0200

    testing feature/roles with new changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,5 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://192.168.2.110:27017/attandance-system-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    useFindAndModify: false
 })
